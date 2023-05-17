f0425736a9bded28752038827caeb15c2e9dabae
Kuldeep Kumar Pal <kuldeep.pal@niser.ac.in>
2023-05-17 19:37:33 +0530

    cp _conda to local site-functions

--- conda-zsh-completion.plugin.zsh
+++ conda-zsh-completion.plugin.zsh
@@ -1,1 +1,8 @@
-fpath+="${0:h}"
+#!/bin/env zsh
+#
+#add following line in you zshrc before loading compinit
+#fpath=($HOME/.local/share/zsh/site-functions $fpath)
+
+DIR=$HOME/.local/share/zsh/site-functions
+mkdir -p $DIR
+cp -v _conda $DIR
